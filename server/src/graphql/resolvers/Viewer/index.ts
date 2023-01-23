361ec10ad8a38dff3fabeac5f8621f002f6ee80f
ViduminiKulathunga <viduminikulathunga@gmail.com>
2023-01-23 09:31:06 +0100

    Added cookies

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,14 +1,23 @@
 import crypto from "crypto";
+import { Response, Request } from "express";
 import { IResolvers } from "@graphql-tools/utils";
 import { Google } from "../../../lib/api";
 import { Viewer, Database, User } from "../../../lib/types";
 import { LogInArgs } from "./types";
 import { SDLValidationContext } from "graphql/validation/ValidationContext";
 
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: true,
+  signed: true,
+  secure: process.env.NODE_ENV === "development" ? false : true,
+};
+
 const logInViaGoogle = async (
   code: string,
   token: string,
-  db: Database
+  db: Database,
+  res: Response
 ): Promise<User | undefined> => {
   const { user } = await Google.logIn(code);
 
@@ -17,7 +26,8 @@ const logInViaGoogle = async (
   }
 
   const userNameList = user.names && user.names?.length ? user.names : null;
-  const userPhotosList = user.photos && user.photos?.length ? user.photos : null;
+  const userPhotosList =
+    user.photos && user.photos?.length ? user.photos : null;
   const userEmailsList =
     user.emailAddresses && user.emailAddresses?.length
       ? user.emailAddresses
@@ -78,6 +88,33 @@ const logInViaGoogle = async (
       listings: [],
     };
   }
+
+  res.cookie("viewer", userId, {
+    ...cookieOptions,
+    maxAge: 365 * 24 * 60 * 60 * 1000,
+  });
+
+  return viewer;
+};
+
+const logInViaCookie = async (
+  token: string,
+  db: Database,
+  req: Request,
+  res: Response
+): Promise<User | undefined> => {
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: req.signedCookies.viewer },
+    { $set: { token } },
+    
+  );
+
+  let viewer: any = updateRes.value;
+
+  if (!viewer) {
+    res.clearCookie("viewer", cookieOptions);
+  }
+
   return viewer;
 };
 
@@ -95,15 +132,15 @@ export const viewResolvers: IResolvers = {
     logIn: async (
       _root: undefined,
       { input }: LogInArgs,
-      { db }: { db: Database }
+      { db, req, res }: { db: Database; req: Request; res: Response }
     ) => {
       try {
         const code = input ? input.code : null;
         const token = crypto.randomBytes(16).toString("hex");
 
         const viewer: User | undefined = code
-          ? await logInViaGoogle(code, token, db)
-          : undefined;
+          ? await logInViaGoogle(code, token, db, res)
+          : await logInViaCookie(token, db, req, res);
 
         if (!viewer) {
           return { didRequest: true };
@@ -120,8 +157,9 @@ export const viewResolvers: IResolvers = {
         throw new Error(`Failed to log in: ${error}`);
       }
     },
-    logOut: () => {
+    logOut: (_root: undefined, _args: {}, { res }: { res: Response }) => {
       try {
+        res.clearCookie("viewer", cookieOptions);
         return { didRequest: true };
       } catch (error) {
         throw new Error(`Failed to log in: ${error}`);
