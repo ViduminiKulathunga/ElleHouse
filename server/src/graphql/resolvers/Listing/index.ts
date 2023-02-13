6c0a9efb823d79983e278403e0d4c956998d4297
ViduminiKulathunga <viduminikulathunga@gmail.com>
2023-02-13 09:30:56 +0100

    listings filter success by price

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -39,6 +39,38 @@ export const listingResolvers: IResolvers = {
         throw new Error(`Failed to query listing: ${error}`);
       }
     },
+    listings: async (
+      _root: undefined,
+      { location, filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.listings.find({});
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 });
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 });
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: ${error}`);
+      }
+    },
   },
   Listing: {
     id: (listing: Listing): string => {
--- server/src/graphql/resolvers/Listing/types.ts
+++ server/src/graphql/resolvers/Listing/types.ts
@@ -27,7 +27,7 @@ export interface ListingsArgs {
 }
 
 export interface ListingsData {
-  region: string | null;
+  region?: string | null;
   total: number;
   result: Listing[];
 }
