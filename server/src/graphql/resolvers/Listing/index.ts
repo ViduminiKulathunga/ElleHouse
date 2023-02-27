0f680daae1c6fbc8b7ea372903c8b52a82f4f701
ViduminiKulathunga <viduminikulathunga@gmail.com>
2023-02-27 04:48:11 +0100

    hostListing mutation success, and hasn't setup cloudainary env variables.

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,8 +1,8 @@
 import { IResolvers } from "@graphql-tools/utils";
 import { Request } from "express";
 import { ObjectId } from "mongodb";
-import { Google } from "../../../lib/api";
-import { Database, Listing, User } from "../../../lib/types";
+import { Google, Cloudinary } from "../../../lib/api";
+import { Database, Listing, User, ListingType } from "../../../lib/types";
 import { authorize } from "../../../lib/utils";
 import {
   ListingArgs,
@@ -16,6 +16,26 @@ import {
   HostListingArgs,
   Order,
 } from "./types";
+
+const verifyHostListingInput = ({
+  title,
+  description,
+  type,
+  price,
+}: HostListingInput) => {
+  if (title.length > 100) {
+    throw new Error("listing title must be under 100 characters");
+  }
+  if (description.length > 5000) {
+    throw new Error("listing description must be under 5000 characters");
+  }
+  if (type !== ListingType.Apartment && type !== ListingType.House) {
+    throw new Error("listing type must be either an apartment or house");
+  }
+  if (price < 0) {
+    throw new Error("price must be greater than 0");
+  }
+};
 
 export const listingResolvers: IResolvers = {
   Query: {
@@ -89,6 +109,50 @@ export const listingResolvers: IResolvers = {
       } catch (error) {
         throw new Error(`Failed to query listings: ${error}`);
       }
+    },
+  },
+  Mutation: {
+    hostListing: async (
+      _root: undefined,
+      { input }: HostListingArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<Listing> => {
+      verifyHostListingInput(input);
+
+      let viewer = await authorize(db, req);
+      if (!viewer) {
+        throw new Error("viewer cannot be found");
+      }
+
+      const { country, admin, city } = await Google.geocode(input.address);
+      if (!country || !admin || !city) {
+        throw new Error("invalid address input");
+      }
+
+      const imageUrl = await Cloudinary.upload(input.image);
+
+      const insertData: any = {
+        _id: new ObjectId(),
+        ...input,
+        image: imageUrl,
+        bookings: [],
+        bookingsIndex: {},
+        country,
+        admin,
+        city,
+        host: viewer._id,
+      };
+
+      await db.listings.insertOne(insertData);
+
+      const insertedListing: Listing = insertData;
+
+      await db.users.updateOne(
+        { _id: viewer._id },
+        { $push: { listings: insertedListing._id } }
+      );
+
+      return insertedListing;
     },
   },
   Listing: {
